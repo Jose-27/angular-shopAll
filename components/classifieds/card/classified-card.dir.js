13f75a617455f4752e4ee17c25beac3729c37d84
Joe <bdj19@hotmail.com>
2017-03-09 00:31:12 -0500

    adding a card directive

---
+++ components/classifieds/card/classified-card.dir.js
@@ -0,0 +1,61 @@
+(function(){
+	"use strict";
+
+
+	angular
+	.module("ngClassifieds")
+	.directive("classifiedCard", function() {
+		return {
+			templateUrl: "components/classifieds/card/classified-card.tpl.html",
+			scope: {
+				classifieds: "=",
+				classifiedsFilter: "=",
+				category: "="
+			},
+			controller: classifiedCardController,
+			controllerAs: 'self'
+		}
+
+		function classifiedCardController($state, $scope, $mdDialog){
+
+			var self = this;
+			self.editClassified = editClassified;
+			self.deleteClassified = deleteClassified;
+
+
+			function editClassified(classified) {
+				$state.go('classifieds.edit', {
+					id: classified.id,
+					classified: classified
+				});
+			}
+
+
+			function deleteClassified(event, classified){
+
+				var confirm = $mdDialog.confirm()
+				.title('Are you sure want to delete' + classified.title + '?')
+				.ok('Yes')
+				.cancel('No')
+				.targetEvent(event);
+				$mdDialog.show(confirm).then(function() {
+					var index = self.classifieds.indexOf(classified);
+					self.classifieds.splice(index, 1);
+				},function(){
+
+
+				});
+			}
+
+			function showToast(message) {
+				$mdToast.show(
+					$mdToast.simple()
+					.content("classified Saved!")
+					.position('top, right')
+					.hideDelay(3000)
+				);
+			}
+
+		}
+	});
+})();
